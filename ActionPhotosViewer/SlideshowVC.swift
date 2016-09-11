0dfad6e51327e2902deb0ae692adc26852802ff1
akshay23 <akshay@animoto.com>
2016-09-10 23:24:57 -0400

    Slideshow works!

--- ActionPhotosViewer/SlideshowVC.swift
+++ ActionPhotosViewer/SlideshowVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Photos
+import Foundation
 
 class SlideshowVC: UIViewController {
 
@@ -16,7 +17,9 @@ class SlideshowVC: UIViewController {
     @IBOutlet var stopButton: UIButton!
     
     var numberOfPhotosToShow: Int!
-    var currentPhotoFetch : PHFetchResult<PHAssetCollection>?
+    var currentShowIndex: Int!
+    var images: [UIImage]?
+    var showTimer: Timer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,24 +44,95 @@ class SlideshowVC: UIViewController {
         navTitle.textAlignment = .center
         navTitle.font = UIFont.systemFont(ofSize: 17)
         navigationItem.titleView = navTitle
-
-        // Get user library collection
-        let fetchOption = PHFetchOptions()
-        fetchOption.sortDescriptors = [NSSortDescriptor(key: "startDate", ascending: true)]
-        fetchOption.predicate = NSPredicate(format: "estimatedAssetCount > 0")
-        currentPhotoFetch = PHAssetCollection.fetchAssetCollections(with: .smartAlbum,
-                                                                    subtype: .smartAlbumUserLibrary,
-                                                                    options: fetchOption)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        images = []
+        currentShowIndex = 0
+        fetchPhotoAtIndexFromEnd(index: 0)
         
+        if let images = images, images.count > 0 {
+            stopButton.isEnabled = true
+            restartButton.isEnabled = true
+            resetTimer()
+            print("Slideshow timer started")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func fetchPhotoAtIndexFromEnd(index: Int) {
+        let imgManager = PHImageManager.default()
+        let fetchResult: PHFetchResult = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+        
+        // Note that if the request is not set to synchronous
+        // the requestImageForAsset will return both the image
+        // and thumbnail; by setting synchronous to true it
+        // will return just the thumbnail
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.isSynchronous = true
+        
+        // If the fetch result isn't empty,
+        // proceed with the image request
+        if fetchResult.count > 0 {
+            // Perform the image request
+            imgManager.requestImage(for: fetchResult.object(at: fetchResult.count - 1 - index) as PHAsset,
+                                    targetSize: slideshowImageView.frame.size,
+                                    contentMode: PHImageContentMode.aspectFill,
+                                    options: requestOptions)
+            {
+                (image, _) in
+                
+                // Add the returned image to your array
+                if let image = image {
+                    self.images!.append(image)
+                }
+                
+                // If you haven't already reached the first
+                // index of the fetch result and if you haven't
+                // already stored all of the images you need,
+                // perform the fetch request again with an
+                // incremented index
+                if index + 1 < fetchResult.count && self.images!.count < self.numberOfPhotosToShow {
+                    self.fetchPhotoAtIndexFromEnd(index: index + 1)
+                } else {
+                    // Else you have completed creating your array
+                    print("Completed array: \(self.images)")
+                }
+            }
+        }
+    }
+    
+    func resetTimer() {
+        showTimer = Timer.scheduledTimer(timeInterval: 1.0,
+                                         target: self,
+                                         selector: #selector(SlideshowVC.showNextPhoto),
+                                         userInfo: nil,
+                                         repeats: true)
+    }
+
+    func showNextPhoto() {
+        slideshowImageView.image = images![currentShowIndex%numberOfPhotosToShow]
+        currentShowIndex! += 1
+    }
+}
+
+// MARK: IBActions
+extension SlideshowVC {
+    @IBAction func restartShow(_ sender: AnyObject) {
+        showTimer.invalidate()
+        currentShowIndex = 0
+        stopButton.isEnabled = true
+        resetTimer()
+    }
+    
+    @IBAction func stopShow(_ sender: AnyObject) {
+        showTimer.invalidate()
+        stopButton.isEnabled = false
     }
 }
